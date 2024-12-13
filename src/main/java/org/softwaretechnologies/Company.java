998aed00fe77f6ef4e4c3d566658b8296f07f368
ekkonovalova <ekkonovalova@edu.tversu.net>
2024-12-13 09:31:16 +0300

    1

--- src/main/java/org/softwaretechnologies/Company.java
+++ src/main/java/org/softwaretechnologies/Company.java
@@ -3,6 +3,7 @@ package org.softwaretechnologies;
 import org.softwaretechnologies.employee.Employee;
 import org.softwaretechnologies.employee.EmployeeType;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,26 +16,16 @@ public class Company {
         this.employeeList = new ArrayList<>();
     }
 
-    /**
-     * Создает и добавляет сотрудника в коллекцию employeeList.
-     * @param name имя работника
-     * @param baseSalary базовая зарплата сотрудника
-     * @param type тип работника
-     */
     public void addEmployee(String name, int baseSalary, EmployeeType type) {
-        // TODO: реализуйте вышеуказанную функцию
-
+        employeeList.add(EmployeeFactory.createEmployee(name, baseSalary, type));
     }
 
-    /**
-     * Возвращает сумму зарплат всех сотрудников за указанный месяц
-     * @param month номер месяца
-     * @return сумма зарплат всех сотрудников за указанный месяц
-     */
     public int getMonthSalary(int month) {
-        // TODO: реализуйте вышеуказанную функцию
-
-        return 0;
+        int summ = 0;
+        for (Employee e:employeeList){
+            summ += e.getMonthSalary(month);
+        }
+        return summ;
     }
 
     public String getName() {
--- src/main/java/org/softwaretechnologies/EmployeeFactory.java
+++ src/main/java/org/softwaretechnologies/EmployeeFactory.java
@@ -1,23 +1,52 @@
 package org.softwaretechnologies;
 
 import org.softwaretechnologies.employee.*;
+import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class EmployeeFactory {
+    private static class Manager extends Employee {
+        protected Manager(String name, int baseSalary) {
+            super(name, baseSalary);
+        }
 
-    /**
-     * Сначала создайте классы, наследуемые от  {@link  org.softwaretechnologies.employee.Employee Employee} для каждого из значений в EmployeeType.
-     * Функция должна создавать и возвращать Employee нужного типа. Тип зависит от значения параметра type.
-     *  name имя сотрудника
-     *  baseSalary базовая зарплата сотрудника
-     *  type тип сотрудника
-     * @return созданного сотрудника нужного типа. Тип зависит от параметра type.
-     */
+        @Override
+        public int getMonthSalary(int month) {
+            if (month %2 == 0){
+                return baseSalary;
+            }
+            return baseSalary/2;
+        }
+    }
 
+    private static class Programmer extends Employee {
+        protected Programmer(String name, int baseSalary) {
+            super(name, baseSalary);
+        }
+
+        @Override
+        public int getMonthSalary(int month) {
+            return baseSalary;
+        }
+    }
+
+    private static class Tester extends Employee {
+        protected Tester(String name, int baseSalary) {
+            super(name, baseSalary);
+        }
+
+        @Override
+        public int getMonthSalary(int month) {
+            return baseSalary * YearMonth.of(LocalDate.now().getYear(), month).lengthOfMonth();
+        }
+    }
 
 
     public static Employee createEmployee(String name, int baseSalary, EmployeeType type) {
-        // TODO: реализуйте вышеуказанную функцию
-
-        return null;
+        return switch (type){
+            case Manager -> new Manager(name, baseSalary);
+            case Programmer -> new Programmer(name, baseSalary);
+            case Tester -> new Tester(name, baseSalary);
+        };
     }
 }
